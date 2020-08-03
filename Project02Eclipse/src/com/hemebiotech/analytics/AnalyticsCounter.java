33eb48e5fec3929a3f8c6c85793c5d1ed16780fa
RedKissiFrott <redkissifrott@yahoo.fr>
2020-08-03 18:34:25 +0200

    Exceptions handling
    add try with resource / catch

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,37 +3,45 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
 
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) {
 		// symptoms counters
 		int headacheCount = 0;
 		int rashCount = 0;
 		int pupilCount = 0;
 		// first get input
-		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			} else if (line.equals("rash")) {
-				rashCount++;
-			} else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		try (BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"))) {
+			String line = reader.readLine();
+			while (line != null) {
+				System.out.println("symptom from file: " + line);
+				if (line.equals("headache")) {
+					headacheCount++;
+					System.out.println("number of headaches: " + headacheCount);
+				} else if (line.equals("rash")) {
+					rashCount++;
+				} else if (line.contains("pupils")) {
+					pupilCount++;
+				}
 
-			line = reader.readLine(); // get another symptom
+				line = reader.readLine(); // get another symptom
+			}
+		} catch (IOException e) {
+			System.out.println("an exception occured while reading file");
+			e.printStackTrace();
 		}
-		reader.close();
 
 		// next generate output
-		FileWriter writer = new FileWriter("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		try (FileWriter writer = new FileWriter("result.out")) {
+			writer.write("headache: " + headacheCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("dialated pupils: " + pupilCount + "\n");
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("an exception occured while writing file");
+			e.printStackTrace();
+		}
 	}
 }
