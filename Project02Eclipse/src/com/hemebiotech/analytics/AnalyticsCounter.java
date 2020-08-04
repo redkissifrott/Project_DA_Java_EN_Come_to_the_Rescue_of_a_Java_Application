b7ebcaf7c8436eea0710118d51f0425d1de30361
RedKissiFrott <redkissifrott@yahoo.fr>
2020-08-04 15:19:07 +0200

    new class for writer

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -13,17 +13,9 @@ public class AnalyticsCounter {
 		// count occurrences
 		CountSymptomsOccurences counter = new CountSymptomsOccurences(symptomsList);
 		Map<String, Integer> symptomsOccurrences = counter.countOccurrences();
-		System.out.println(symptomsOccurrences);
 
 		// next generate output
-//		try (FileWriter writer = new FileWriter("result.out")) {
-//			writer.write("headache: " + headacheCount + "\n");
-//			writer.write("rash: " + rashCount + "\n");
-//			writer.write("dialated pupils: " + pupilCount + "\n");
-//			writer.close();
-//		} catch (IOException e) {
-//			System.out.println("an exception occured while writing file");
-//			e.printStackTrace();
-//		}
+		ISymptomsOccurrencesOutput writer = new WriteSymptomsOccurencesInFile("result.out");
+		writer.outputSymptomsOccurrences(symptomsOccurrences);
 	}
 }
