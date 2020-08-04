fe4db40fdb82eb64bf326bdb0ac9d1e65218e735
RedKissiFrott <redkissifrott@yahoo.fr>
2020-08-04 10:40:25 +0200

    Filepath setter in reader
    Check if source file exists and is text type file

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -3,6 +3,9 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,24 +16,41 @@ import java.util.List;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+
 	/**
 	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
+	public ReadSymptomDataFromFile(String filepath) {
+		this.setFilepath(filepath);
 	}
-	
+
+	/**
+	 * test if file exists and if it's a text type file; if not, throw exception
+	 * 
+	 * @param filepath
+	 */
+	public void setFilepath(String filepath) {
+		Path path = Paths.get(filepath);
+		if (!Files.exists(path)) {
+			throw new RuntimeException("This data source file does not exist or is not accessible");
+		} else if (!(filepath.endsWith(".text") | filepath.endsWith(".txt"))) {
+			throw new RuntimeException("Tha data source must be text type file (.txt or .text)");
+		} else {
+			this.filepath = filepath;
+		}
+	}
+
 	@Override
 	public List<String> getSymptoms() {
 		ArrayList<String> result = new ArrayList<String>();
-		
+
 		if (filepath != null) {
 			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));
 				String line = reader.readLine();
-				
+
 				while (line != null) {
 					result.add(line);
 					line = reader.readLine();
@@ -40,7 +60,7 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return result;
 	}
 
